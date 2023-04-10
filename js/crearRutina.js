831960f8fd133a1f683b1dd6b4fab03c340ea9ae
BGM <bastian.garcia.m2@gmail.com>
2023-04-10 15:32:20 -0400

    asd

--- js/crearRutina.js
+++ js/crearRutina.js
@@ -1,6 +1,7 @@
 const form = document.querySelector(".form");
 const fieldBtn = document.querySelector("#field-btn");
 const btn = document.querySelector(".btn-add");
+const btnPublicar = document.querySelector("#btn-publicar");
 var counter = 2;
 
 btn.addEventListener("click", () => {
@@ -11,18 +12,25 @@ btn.addEventListener("click", () => {
   newspan.innerHTML = "Ejercicio " + counter;
 
   let newInput = document.createElement("input");
+  newInput.id= "nombre-ejercicio-" + counter;
 
   let newSpanInputNumber = document.createElement("span");
   newSpanInputNumber.innerHTML = "Numero de series";
   let newInputNumber = document.createElement("input");
+  newInputNumber.type = "number";
+  newInputNumber.id = "series-ejercicio-" + counter;
 
   let newSpanInputNumberRep = document.createElement("span");
   newSpanInputNumberRep.innerHTML = "Rango de repeticiones";
   let newInputNumberRep = document.createElement("input");
+  newInputNumberRep.type = "number";
+  newInputNumberRep.id = "repeticiones-ejercicio-" + counter;
+
 
   counter++;
 
   form.removeChild(fieldBtn);
+  form.removeChild(btnPublicar);
 
   newDiv.appendChild(newspan);
   newDiv.appendChild(newInput);
@@ -34,4 +42,9 @@ btn.addEventListener("click", () => {
   newDiv.appendChild(newInputNumberRep);
   form.appendChild(newDiv);
   form.appendChild(fieldBtn);
+  form.appendChild(btnPublicar);
+});
+
+btnPublicar.addEventListener("click", () => {
+
 });
